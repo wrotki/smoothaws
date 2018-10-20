c2475e93400e9984cd5fb42e4da33eccdb3b3ace
Mariusz Borsa <mborsa@polyverse.io>
2018-10-20 12:59:12 +0200

    Addest a test invoking Batch jobdef upsert

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,7 +1,11 @@
 package test.utest.examples.examples
 
+import cats.effect._
+import cats.syntax.all._
 import utest._
 import org.company.lib.Lib
+import particles.AWS.Batch
+
 
 object HelloTests extends TestSuite{
   val tests = this{
@@ -14,6 +18,19 @@ object HelloTests extends TestSuite{
     'test3{
       val a = List[Byte](1, 2)
       //a(10)
+    }
+    'testBatchJobDefUpsert{
+
+      // https://www.beyondthelines.net/computing/scala-future-and-execution-context/
+      implicit val executor =  scala.concurrent.ExecutionContext.global
+
+        val res = Batch.registerJobDefIfMissing("ParticleBatchRegisterJobDefinitionTest")
+        res.unsafeRunAsync(
+          cb => {
+            println("Batch.registerJobDefIfMissing complete")
+          }
+        )
+      res.unsafeToFuture()
     }
   }
 }
